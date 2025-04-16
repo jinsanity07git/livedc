4638e861e57f2d9637fa56763cba0aa0766dec83
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2025-04-16 15:18:29 -0400

    [enhan] fact check with fat-burning zone

--- src/livedc/health/workout.py
+++ src/livedc/health/workout.py
@@ -7,21 +7,24 @@ from ..desc import add_rdoc
 def fat_burning_heart_rate(age):
 
     """
-    The fat-burning zone is typically 50-70% of the maximum heart rate, which is calculated as:
+    The fat-burning zone is typically 70-80% of the maximum heart rate, which is calculated as:
     $$ \\text{Maximum Heart Rate} = 220 - \\text{Age} $$
     The lower and upper bounds of the fat-burning heart rate range are then calculated as:
-    $$ \\text{Lower Bound} = \\text{Maximum Heart Rate} \\times 0.5 $$
-    $$ \\text{Upper Bound} = \\text{Maximum Heart Rate} \\times 0.7 $$
+    $$ \\text{Lower Bound} = \\text{Maximum Heart Rate} \\times 0.7 $$
+    $$ \\text{Upper Bound} = \\text{Maximum Heart Rate} \\times 0.8 $$
 
     Args:
         age (int): Age of the person.
         
     Returns:
         tuple: A tuple containing the lower and upper bounds of the heart rate range.
+    
+    Reference:
+    * [Cracking the Code: Heart Rate Zones for Fat Burning](https://www.miramont.us/blog/heart-rate-to-burn-fat)
     """
     max_heart_rate = 220 - age
-    lower_bound = max_heart_rate * 0.5
-    upper_bound = max_heart_rate * 0.7
+    lower_bound = max_heart_rate * 0.7
+    upper_bound = max_heart_rate * 0.8
     return lower_bound, upper_bound
 
 
