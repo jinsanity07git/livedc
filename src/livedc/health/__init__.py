eedb7b394b792c0c53fe5a43d2583405b4960dc5
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2025-04-17 14:33:19 +0000

    [doc] add HR zone reference

---
+++ src/livedc/health/__init__.py
@@ -0,0 +1,24 @@
+"""
+ a nice summary in Samsung Health based on the information from American College of Sport Medicine.
+
+Zone 5 — Maximum: 90%-100% of max HR
+
+Good for short bursts of 1 or 2 minutes. Improves athletic performance. There's a high risk for injury, so stay away from this zone unless you're a competitive athlete
+
+Zone 4 — Anaerobic: 80%-90% of max HR
+
+Improves endurance and general fitness. Breathing is laboured. This level is recommended for short spurts only 2 or 3 days a week.
+
+Zone 3 — Aerobic: 70%-80% of max HR
+
+Intense but not exhausting Best zone for weight loss because the total calones burnt is higher than in the weight control zone. Great for burning fat, strengthening muscles, and general fitness. Burns 50% carbs and 50% fat
+
+Zone 2 — Weight control: 60%-70% of max HR
+
+Slow to medium-paced exercise. 65% of kcal. burnt in this zone are fat.
+
+Zone 1 — Low intensity: 50%-60% of max HR
+
+Strengthens your heart and reduces body fat. cholesterol, and blood pressure, but doesn't increase endurance or strength. Good for beginners.
+
+"""
